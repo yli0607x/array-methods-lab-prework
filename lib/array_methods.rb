6b7feeb954381fbf4bd8a1a3e606cd85b76807a8
Yu Li <yli0607x@gmail.com>
2018-10-04 23:55:37 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
-def using_include(array, element)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
+def using_include(array, element)
+  array.include?("wow")
+  array.include?("Tardis")
 end
 
 def using_sort(array)
